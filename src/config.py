74f5f71ed260cd2a943836eec6d470b4eb69a1d4
levanpon98 <levanpon98@gmail.com>
2020-10-09 19:22:52 +0700

    fit config.py

--- src/config.py
+++ src/config.py
@@ -11,13 +11,8 @@ dff = 1024
 top_k = 5000
 vocab_size = top_k + 1
 features_shape = 2048
-<<<<<<< HEAD
 attention_features_shape = 81
 data_path = '../../xraydata/data'
-=======
-attention_features_shape = 64
-data_path = '../data'
->>>>>>> develop
 EPOCHS = 20
 print_every = 40
 max_length = 173
